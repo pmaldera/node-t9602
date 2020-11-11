0fb6f6b039f9f2c9c9914ee12def214903427c86
Pierre MALDERA <pierremaldera@hotmail.fr>
2020-11-11 12:53:02 +0100

    Added README.md

--- examples/index.js
+++ examples/index.js
@@ -1,4 +1,4 @@
 var T9602 = require('../lib');
 var sensor = new T9602();
-console.log(sensor.getTemperature());
-console.log(sensor.getHumidity());
+console.log('Temperature : ' + sensor.getTemperature() + ' °C');
+console.log('Humidity : ' + sensor.getHumidity() + ' %');
