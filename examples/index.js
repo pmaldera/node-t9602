34df2a6fdd8218e67e1d22b0a8dd083d3a86c0ac
Pierre MALDERA <pierremaldera@hotmail.fr>
2020-11-12 16:36:57 +0100

    updated js example

--- examples/index.js
+++ examples/index.js
@@ -1,4 +1,4 @@
-var T9602 = require('../lib');
+var T9602 = require('../lib').T9602;
 var sensor = new T9602();
 console.log('Temperature : ' + sensor.getTemperature() + ' °C');
 console.log('Humidity : ' + sensor.getHumidity() + ' %');
